a8003d3caf57d3ead2776fa56826a8e51e7ebcb9
majeshmd <majesh.hyb@gmail.com>
2020-04-15 10:37:07 +0530

    WPF_Project

---
+++ MessageEventAggregator/MessageEventAggregator/MessageEventAggregator.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageEventAggregator
+{
+    public class MessageEventAggregator : PubSubEvent<string>{}
+}
+    
